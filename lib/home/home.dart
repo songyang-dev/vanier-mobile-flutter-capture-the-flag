72e8b45a787fd3669f24d0b4ed40830f33443494
Song Yang <songyang.dev@gmail.com>
2023-08-28 21:55:57 -0400

    Implement scrolling ListView

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -5,10 +5,42 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Here is another one. FLAG flutter FLAG"),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Scroll down to Item #25"),
       ),
+      body: ListView.builder(
+        itemCount: 30,
+        itemBuilder: (context, index) {
+          if (index == 26) return const TweetCard();
+
+          return ListItem(title: index.toString());
+        },
+      ),
+    );
+  }
+}
+
+class ListItem extends StatelessWidget {
+  const ListItem({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(title: Text("Item #$title")),
+    );
+  }
+}
+
+class TweetCard extends StatelessWidget {
+  const TweetCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Image.asset('assets/tweet.png'),
     );
   }
 }
